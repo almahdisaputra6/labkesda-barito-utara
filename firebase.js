82bc6f2dc3cb06d719aa4499b1051248fe85c58f
almahdisaputra6 <almahdisaputra6@gmail.com>
2026-03-26 15:26:14 +0700

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyAesZs1ytBEiE0_UW1eXwQtM5lXbE_VhJs",
   authDomain: "labkesda-barito-utara.firebaseapp.com",
   projectId: "labkesda-barito-utara",
-  storageBucket: "labkesda-barito-utara.firebasestorage.app",
+  storageBucket: "labkesda-barito-utara.appspot.com",
   messagingSenderId: "755322490739",
   appId: "1:755322490739:web:4f9f4b240d630639b87f06"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
 const auth = firebase.auth();
+const db = firebase.firestore();
 const storage = firebase.storage();
